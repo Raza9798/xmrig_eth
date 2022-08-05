8e1ec14c91b13340b412f20e5a95ed2f7cd9d066
Mohamed Raza <59387235+Raza9798@users.noreply.github.com>
2022-08-05 18:19:11 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,4 @@
+sudo apt-get update && sudo apt-get install wget -y
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-focal-x64.tar.gz && tar -xf xmrig-6.18.0-focal-x64.tar.gz
+cd xmrig-6.18.0
+sudo nano config.json
